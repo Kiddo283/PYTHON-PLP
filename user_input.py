030c9d4be5381daaab17e18931d31a0c9d0adb12
Kiddo283 <160440496+Kiddo283@users.noreply.github.com>
2024-02-28 10:43:49 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,9 @@
+#user_input.py
+#asking for user name
+name= input("What is your name?")
+#asking for user age
+age= input ("How old are you?")
+#asking for user location
+location= input("Where do you live?")
+#Print personalized message
+print(f"Hello, {name}, you are {age} years old and live in {location}")
